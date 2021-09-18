72735db9d99d414ed1f3a893f03934bf0832bda0
Gmatieso <matiesogeoffrey@gmail.com>
2021-09-18 22:34:26 +0300

    created the  askQuestion function that accepts explicit intent and converts the editText1 to text to String and store in a variable called myString

--- app/src/main/java/com/example/intents/MainActivity.kt
+++ app/src/main/java/com/example/intents/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.intents
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var editText1: TextView  // initializing the myString variable
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        editText1 = findViewById(R.id.editText1)
+    }
+    fun askQuestion(view: View) {
+        val i = Intent(this,ActivityB::class.java)
+       val  myString  = editText1.text.toString()
     }
 }
