e53a262a14cd21748794d72f5a89ca1a87499e65
Gmatieso <matiesogeoffrey@gmail.com>
2021-09-19 00:04:20 +0300

    Added codes for returning back the text message when ask question button is pressed

--- app/src/main/java/com/example/intents/ActivityB.kt
+++ app/src/main/java/com/example/intents/ActivityB.kt
@@ -2,10 +2,18 @@ package com.example.intents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.content.Intent
+import android.widget.TextView
 
 class ActivityB : AppCompatActivity() {
+    lateinit var textView2: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_b)
+
+        val  extras = intent.extras ?:return
+        val qString = extras.getString("qString")
+        textView2.text = qString
     }
 }
